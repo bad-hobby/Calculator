fa27d66fa21776adfe3507da8a13e84d8bc8f808
Tedo <thdrmarinov@abv.bg>
2019-11-17 12:11:44 +0200

    Added compute function, met an error, equals button cannot execute properly

--- script.js
+++ script.js
@@ -23,25 +23,49 @@ class Calculator {
     }
 
     chooseOperation(operation) {
-        if (this.currentOperand === '') return;
+        if (this.currentOperand === '') return; // when curr op is empty, clear prev op field wont execute
         if (this.previousOperand !== '') {
             this.compute();
-        }
-        this.operation = operation;
-        this.previousOperand = this.currentOperand;
-        this.currentOperand = '';
+        } // if prev op isnt empty - compute with clicking any operation button instead of equals button 
+        this.operation = operation; // sets the operation to the onae passed in
+        this.previousOperand = this.currentOperand; // sends current op above
+        this.currentOperand = ''; // clears current op value
     }
 
     compute() {
-
+        let result;
+        const prev = parseFloat(this.previousOperand); // converting prev string to number
+        const current = parseFloat(this.currentOperand); // converting current string to number
+        if (isNaN(previous) || isNaN(current)) return; // if we dont have any of these two values entered - cancels function with return
+        switch (this.operation) {
+            case '+' : 
+              result = prev + current; 
+              break;
+            case '-' : 
+              result = prev - current; 
+              break;
+            case '*' : 
+              result = prev * current; 
+              break;
+            case '÷' : 
+              result = prev / current; 
+              break;
+            default : 
+              return; // Switch through operations and compute each case. If none of these 4 operations is entered - return 
+        }
+        this.currentOperand = result; // sets curr op field to the result
+        this.operation = undefined; // clears operation
+        this.previousOperand = ''; // empty prev op field
     }
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand; 
-        // Set the text in the white to text of the clicked button
+        // Set the text in the current op to text of the clicked button
         this.previousOperandTextElement.innerText = this.previousOperand; 
+        // set the text in the prev op field to the prev op text
     }
 }
+//-------------------------------------------------------------------------------------------------------------------
 // Get HTML elements in variables
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
@@ -66,3 +90,8 @@ operationButtons.forEach(button => {
         calculator.updateDisplay();
     })
 });
+// Equals button functionality
+equalsButton.addEventListener('click', button => {
+    calculator.compute(); // when equals button is selected it calls the compute function
+    calculator.updateDisplay();
+})
